2ffd164bf680e76f7120b0569fbed7a9e5aab34f
smiley741 <smiley_twos@hotmail.com>
2019-10-19 03:53:45 +0700

    dynamic web

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("hi");
+console.log("hello world");
+
+function doit(barId, crId) {
+  const htmlbar = document.getElementById(barId);
+  const htmlbarcr = document.getElementById(crId);
+  htmlbarcr.style.width = htmlbar.value + "%";
+}
+
+function clickChange() {
+  const nameC = document.getElementById("nameCID")
+  const realname = document.getElementById("realname")
+  realname.innerText = nameC.value
+  nameC.value = ""
+}
+
+function clickclickboom() {
+  const skillinput = document.getElementById("skillButt");
+  const ulID = document.getElementById("ulId");
+  
+  const firstli = document.createElement("li");
+  const elementId  = "li" + ulID.children.length;
+  firstli.setAttribute("id", elementId);
+  firstli.innerHTML = '<div class="skillText">' + 
+                      skillinput.value + 
+                      '<button onclick="functionD(\'' + elementId + '\')">delete</button>' +
+                      '</div>';
+  ulID.appendChild(firstli);
+  skillinput.value=""
+}
+
+function functionD(elementId) {
+  document.getElementById(elementId).remove()
+}
+
+function BGC(colorcode){
+ document.body.style.background = colorcode
+} 
